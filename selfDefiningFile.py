7525437f27ab201a4c29009101aaf97ea17e10fc
tqb-web <1797834904@qq.com>
2023-11-20 20:14:11 +0800

    更新

--- selfDefiningFile.py
+++ selfDefiningFile.py
@@ -12,3 +12,25 @@ with open('./selfDefiningTxt.txt', 'w', encoding='utf-8') as f:
     for name in movieNameLst:
         f.write(name + ' 100 tqb')
         f.write('\n')
+
+
+def generate_fibonacci(n):
+    fibonacci_sequence = [0, 1]  # 初始的斐波那契序列
+
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+
+    while len(fibonacci_sequence) < n:
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
+
+    return fibonacci_sequence
+
+# 打印前 10 个斐波那契数
+n = 10
+fibonacci_sequence = generate_fibonacci(n)
+print(f"前 {n} 个斐波那契数列为: {fibonacci_sequence}")
